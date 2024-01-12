eea8cf731d71f8e87006c31418ec92917a9e2724
Akshay Birajdar <akshaybirajdar05@gmail.com>
2024-01-13 01:07:31 +0530

    add sidekiq start script

---
+++ start_sidekiq.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# Get the value of n from the environment or use a default value of 2
+n=${N:-2}
+
+# Array to store the PIDs
+pids=()
+
+# Function to start sidekiq process and capture the PID
+start_sidekiq() {
+    bundle exec sidekiq -c 4 -r ./sidekiq_test/a_job.rb > /dev/null 2>&1 &
+    pid=$!
+    pids+=($pid)
+    echo "Started sidekiq process with PID: $pid"
+}
+
+# Loop to start n sidekiq processes
+for ((i=1; i<=n; i++)); do
+    start_sidekiq
+done
+
+echo "Started $n sidekiq processes with PIDs: ${pids[@]}"
+
